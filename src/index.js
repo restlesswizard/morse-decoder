c186fdbec1ce9e38218eef6bb20b996028bd9c8d
restlesswizard <erxme@yandex.ru>
2023-01-29 04:07:29 +0500

    feat: finish morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,83 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let exprToArray = expr.split('');
+  let arrOfChunks = []
+  
+  const chunkSize = 10;
+  for (let i = 0; i < exprToArray.length; i+=chunkSize) {
+    let chunk = exprToArray.slice(i, i + chunkSize);
+    arrOfChunks.push(chunk)
+  }
+
+  let digitsToArray = []
+  
+  arrOfChunks.map((elem) => {
+    let digits = elem.join('');
+    digitsToArray.push(digits)
+  })
+
+  let digitsToMorse = []
+  
+  digitsToArray.map((elem) => {
+    const chunkSize = 2
+    for (let j = 0; j < elem.length; j+=chunkSize) {
+      let chunk = elem.slice(j, j + chunkSize);
+      if (chunk === '00') {
+        digitsToMorse.push('L')
+      }
+      if (chunk === '**') {
+        digitsToMorse.push('S')
+      }
+      if (chunk === '10') {
+        digitsToMorse.push('.')
+      }
+      if (chunk === '11') {
+        digitsToMorse.push('-')
+      }
+    }
+  })
+
+  let formSymbols = [];
+
+  const symbolSize = 5
+  for (let x = 0; x < digitsToMorse.length; x += symbolSize) {
+    let symbol = digitsToMorse.slice(x, x + symbolSize)
+    formSymbols.push(symbol)
+  }
+
+  let morseCode = [];
+  formSymbols.map((elem) => {
+    let symbolCode = elem.join('');
+    
+    if (symbolCode.includes('S')) {
+      let editedSymbol = symbolCode.replace(/[S]+/g, ' ')
+      morseCode.push(editedSymbol)
+    } else if (symbolCode.includes('L')) {
+      let editedSymbol = symbolCode.replace(/[L]+/g, '')
+      morseCode.push(editedSymbol)
+    } else {
+      morseCode.push(symbolCode)
+    }
+  })
+
+  let morseTable = Object.entries(MORSE_TABLE)
+
+  let decodedString = []
+  morseCode.map((elem) => {
+    if (elem === ' ') {
+      decodedString.push(elem)
+    } else 
+		{
+      for (let y = 0; y < morseTable.length; y++) {
+				if (elem === morseTable[y][0]) {
+					decodedString.push(morseTable[y][1])
+				}
+    	}
+    }
+  })
+
+  return decodedString.join('')
 }
 
 module.exports = {
